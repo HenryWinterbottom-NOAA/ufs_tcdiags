36e517e80cb18b178cfefc6781340d82279ff463
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-10 13:56:36 -0700

    Added exceptions.

---
+++ ush/exceptions.py
@@ -0,0 +1,82 @@
+# =========================================================================
+
+# Module: ush/exceptions.py
+
+# Author: Henry R. Winterbottom
+
+# Email: henry.winterbottom@noaa.gov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the respective public license published by the
+# Free Software Foundation and included with the repository within
+# which this application is contained.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# =========================================================================
+
+"""
+Module
+------
+
+    exceptions.py
+
+Description
+-----------
+
+    This module loads the exceptions package.
+
+Classes
+-------
+
+    TCDiagsError()
+
+        This is the base-class for exceptions encountered within the
+        ush/tcdiags module; it is a sub-class of Error.
+
+Requirements
+------------
+
+- ufs_pytils; https://github.com/HenryWinterbottom-NOAA/ufs_pyutils
+
+Author(s)
+---------
+
+    Henry R. Winterbottom; 10 March 2023
+
+History
+-------
+
+    2023-03-10: Henry Winterbottom -- Initial implementation.
+
+"""
+
+# ----
+
+from utils.error_interface import Error
+
+# ----
+
+# Define all available attributes.
+__all__ = ["TCDiagsError"]
+
+# ----
+
+__author__ = "Henry R. Winterbottom"
+__maintainer__ = "Henry R. Winterbottom"
+__email__ = "henry.winterbottom@noaa.gov"
+
+# ----
+
+
+class TCDiagsError(Error):
+    """
+    Description
+    -----------
+
+    This is the base-class for exceptions encountered within the
+    ush/tcdiags module; it is a sub-class of Error.
+
+    """
